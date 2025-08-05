14fdf46cbab2dacfb543be861947d2b9d76a2a67
Abel García <65343232+abelgarcia2@users.noreply.github.com>
2025-08-05 23:53:42 +0200

    use getopts + add first_boot_script_filename (insert-answer-file.sh)

--- insert-answer-file.sh
+++ insert-answer-file.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 docker_image_name="proxmox-auto-install-assistant"
-iso_filename="$1"
+iso_filename=""
 answer_filename="answer.toml"
+first_boot_script_filename=""
 
 help() {
-    echo "Usage: $0 [options] [filename]"
+    echo "Usage: $0 [options]"
     echo
     echo "Description:"
-    echo "  This script adds and answer.toml response file to a Proxmox VE ISO,"
-    echo "  which can be specified as an argument.. If no ISO file is specified,"
+    echo "  This script adds an answer.toml response file to a Proxmox VE ISO,"
+    echo "  which can be specified as an option. If no ISO file is specified,"
     echo "  it will be downloaded from the Proxmox website"
     echo
     echo "Options:"
-    echo "  -h, --help   Shows that message and exit"
-    echo
-    echo "Arguments:"
-    echo "  filename   Proxmox VE ISO file"
+    echo "  -f    Proxmox VE ISO file"
+    echo "  -h    Shows that message and exit"
+    echo "  -s    First boot script"
 }
 
 get_current_proxmox_version() {
@@ -55,10 +55,19 @@ verify_iso() {
 }
 
 
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    help
-    exit
-fi
+while getopts "hf:s:" opt; do
+    case $opt in
+        h) help; exit ;;
+        f) iso_filename=${OPTARG} ;;
+        s) first_boot_script_filename=${OPTARG} ;;
+        *) help; exit ;;
+    esac
+done
+
+# if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+#     help
+#     exit
+# fi
 
 if ! command -v docker &> /dev/null; then
     echo -e "\033[1;31m[!] docker is not installed. Please install docker\033[0m"
@@ -70,7 +79,7 @@ if [ ! -e $answer_filename ]; then
     exit 1
 fi
 
-if [[ ! -e $iso_filename || $# -eq 0 ]]; then
+if [ ! -e "$iso_filename" ]; then
     echo -e "\033[32m[!] Looks like you still don’t have the ISO image, let’s downloading it...\033[0m"
     download_iso
     verify_iso
@@ -83,5 +92,12 @@ if ! docker image inspect  "$docker_image_name" &> /dev/null; then
 fi
 
 echo -e "\033[32m[!] Now we're going to include the answer file in the iso image...\033[0m"
-docker run --rm -it -v .:/root/proxmox-auto-install "$docker_image_name" \
-    prepare-iso "$iso_filename" --fetch-from iso --answer-file "$answer_filename"
+if [ -n "$first_boot_script_filename" ]; then
+    docker run --rm -it -v .:/root/proxmox-auto-install "$docker_image_name" \
+        prepare-iso "$iso_filename" --fetch-from iso \
+        --answer-file "$answer_filename" --on-first-boot "$first_boot_script_filename"
+else
+    docker run --rm -it -v .:/root/proxmox-auto-install "$docker_image_name" \
+        prepare-iso "$iso_filename" --fetch-from iso \
+        --answer-file "$answer_filename"
+fi
